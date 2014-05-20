b2ecaffd84dc0d174d8e2fa35754fe4fca6cabe7
Paul McElhinney <paulmcelhinney@Pauls-MacBook-Pro.local>
2014-05-20 18:01:10 -0400

    added test class

---
+++ PGMTest.h
@@ -0,0 +1,13 @@
+//
+//  PGMTest.h
+//  Git Test
+//
+//  Created by Paul McElhinney on 5/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGMTest : NSObject
+
+@end
---
+++ PGMTest.m
@@ -0,0 +1,13 @@
+//
+//  PGMTest.m
+//  Git Test
+//
+//  Created by Paul McElhinney on 5/20/14.
+//
+//
+
+#import "PGMTest.h"
+
+@implementation PGMTest
+
+@end
